d07d2aed44e6fd3f9f1022f1a3dca96e94869e18
Chanaka Eshan <78550278+chanakaeshan@users.noreply.github.com>
2023-07-31 18:14:07 +0530

    Add files via upload

---
+++ C# Practicals/lab05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //Q3.
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            array.myArray();
+
+            Console.ReadLine();
+        }
+
+    }
+}
